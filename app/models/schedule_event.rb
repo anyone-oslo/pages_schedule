0218d401a79a00222d90902827050ef0b90eb611
inge <inge@a47e93f7-a817-0410-9d0b-c952cbe0a5d1>
2009-04-03 14:00:56 +0000

    farojournalen: Kalender

--- app/models/schedule_event.rb
+++ app/models/schedule_event.rb
@@ -2,6 +2,12 @@ class ScheduleEvent < ActiveRecord::Base
 	belongs_to_image :image
 	validates_presence_of :name, :starts_at, :ends_at
 	
+	validate do |schedule_event|
+		if !schedule_event.ends_at || schedule_event.ends_at < schedule_event.starts_at
+			schedule_event.ends_at = schedule_event.starts_at
+		end
+	end
+	
 	class << self
 		def years
 			self.find_by_sql('SELECT DISTINCT YEAR(starts_at) AS `starts_at_year` FROM `schedule_events` ORDER BY `starts_at_year` ASC').map{|se| se.starts_at_year}.sort.mapped.to_i
@@ -14,6 +20,22 @@ class ScheduleEvent < ActiveRecord::Base
 			}.merge(options)
 			self.find(:all, find_options)
 		end
+		
+		# Find upcoming events
+		def find_upcoming(options={})
+			find_options = {
+				:conditions => ['published = 1 AND ends_at >= ?', Time.now],
+				:order      => 'starts_at ASC'
+			}.merge(options)
+
+			if find_options.has_key?(:within)
+				find_options[:conditions].first += " AND ends_at <= ?"
+				find_options[:conditions] << (Time.now + find_options[:within])
+				find_options.delete(:within)
+			end
+			
+			self.find(:all, find_options)
+		end
 	end
 	
 	def year
@@ -21,7 +43,7 @@ class ScheduleEvent < ActiveRecord::Base
 	end
 
 	def upcoming?
-		(Time.now < self.starts_at) ? true : false
+		(Time.now < self.ends_at) ? true : false
 	end
 	
 	def past?
@@ -30,6 +52,22 @@ class ScheduleEvent < ActiveRecord::Base
 	
 	def today?
 		(self.starts_at.to_date == Date.today) ? true : false
+	end
+	
+	def duration
+		self.ends_at - self.starts_at
+	end
+
+	# Does this event have a duration?
+	def duration?(options={})
+		options[:fuzziness] ||= 60.seconds
+		(self.duration > options[:fuzziness]) ? true : false
+	end
+	
+	def link
+		url = self[:link]
+		url = "http://#{url}" unless url =~ /^https?:\/\//
+		url
 	end
 
 end
